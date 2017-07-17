74499670c83ce21f2ebac40f0884b8eb4de93baf
9291Sam <9291Sam@users.noreply.github.com>
2017-07-17 15:09:05 -0400

    Add files via upload

--- InstallMinecraft.sh
+++ InstallMinecraft.sh
@@ -1,1 +1,11 @@
-
+#/bin/bash
+sudo apt-get install software-properties-common
+sudo apt-add-repository ppa:webupd8team/java
+cd ~/Documents
+mkdir Games
+cd Games
+wget -q http://s3.amazonaws.com/Minecraft.Download/launcher/Minecraft.jar
+sudo apt-get install oracle-java8-installer
+sudo apt-get update -y; sudo apt-get upgrade -y;
+cd ~/Desktop
+wget Minecraft.desktop
