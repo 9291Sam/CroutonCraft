ca3e613f5b93db6e82e7cbc6796d163168ac457f
9291Sam <9291Sam@users.noreply.github.com>
2017-12-23 11:44:16 -0500

    Update InstallMinecraft.sh

--- InstallMinecraft.sh
+++ InstallMinecraft.sh
@@ -1,9 +1,9 @@
 #/bin/bash
-sudo apt-get install software-properties-common
-sudo apt-add-repository ppa:webupd8team/java
-cd ~/Documents
+sudo apt-get install software-properties-common  #Install REPO handler
+sudo apt-add-repository ppa:webupd8team/java # Install java REPO
+cd ~/Documents 
 mkdir Games
 cd Games
-wget -q http://s3.amazonaws.com/Minecraft.Download/launcher/Minecraft.jar
-sudo apt-get install oracle-java8-installer
+wget -q http://s3.amazonaws.com/Minecraft.Download/launcher/Minecraft.jar #Get the Minecraft Launcher
+sudo apt-get install oracle-java8-installer #Install Java
 sudo apt-get update -y; sudo apt-get upgrade -y;
